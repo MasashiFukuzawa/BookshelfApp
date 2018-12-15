93bcbc4105e44c0b2c72528e7dc29235a9753feb
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2018-12-15 23:29:38 +0900

    Change from .erb to .slim

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module BookshelfApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.generators.template_engine = :slim
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
