2e84b7511553dbd7173d23248cff6768622c140f
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2018-12-15 14:23:49 +0900

    Add bootstrap gem

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,31 +3,31 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "| Bookshelf App"
+    @base_title = "Bookshelf App"
   end
 
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", "Home #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help #{@base_title}"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About #{@base_title}"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact #{@base_title}"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
