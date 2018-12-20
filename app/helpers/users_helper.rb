003ad16cebc9261453615ecf7b020aa1904ec4f7
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2018-12-20 17:04:51 +0900

    Finished user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  def gravatar_for(user, options = {size: 80})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "card-img-top img-fluid img-thumbnail")
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
   
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
+                      password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
