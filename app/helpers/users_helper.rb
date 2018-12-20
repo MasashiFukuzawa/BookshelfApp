41fa75b7277ac95277051304d4370dccf24ce277
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2018-12-20 17:18:25 +0900

    A minor modification for SSL

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
   def gravatar_for(user, options = {size: 80})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "card-img-top img-fluid img-thumbnail")
   end
 
