a36c42ec662263a5d7a80e3b3fa1b58a6de5fc0e
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2018-12-21 19:16:34 +0900

    Rearrangement the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  # コールバックを定義
   before_save {self.email = email.downcase}
-  validates :name, presence: true, length: {maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255},
-                    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  # メソッドを定義
   has_secure_password
+  # バリデーションを定義
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: Constants::VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 6}
 end
