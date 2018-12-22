e1c44ab10fec8c0ba0227bd969f7d4ac956a4b29
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2018-12-22 13:08:58 +0900

    [WIP]Setting permanent cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,30 @@
 class User < ApplicationRecord
-  # コールバックを定義
   before_save {self.email = email.downcase}
   
-  # バリデーションを定義
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},
                     format: {with: Constants::VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 6}
 
-  # Railsメソッドの呼出し
   has_secure_password
-
-  # メソッドを定義
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ?
+  
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ?
       BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+      BCrypt::Password.create(string, cost: cost)
+    end
+    
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  attr_accessor :remember_token
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
