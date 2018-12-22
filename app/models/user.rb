240de5f59e33983a67d9300c5938cfe169025da7
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2018-12-23 02:09:13 +0900

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,13 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,14 +5,22 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests
-  # in alphabetical order.
   fixtures :all
 
-  # テストユーザーがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
   end
 
-  # Add more helper methods to be used by all tests here...
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
