31a1c8903cd461c246b8f1163a49218e3836a0b8
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2018-12-25 07:18:00 +0900

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :update, :edit]
 end
---
+++ test/integration/password_resets_test.rb
@@ -0,0 +1,68 @@
+require 'test_helper'
+
+class PasswordResetsTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+    @user = users(:michael)
+  end
+  
+  test "password reset" do
+    get new_password_reset_path
+    assert_template 'password_resets/new'
+    # メールアドレスが無効な場合
+    post password_resets_path(@user), params: {password_reset: {email: ""}}
+    assert_not flash.empty?
+    assert_template 'password_resets/new'
+    # メールアドレスが有効な場合
+    post password_resets_path(@user), params: {password_reset: {email: @user.email}}
+    assert_not_equal @user.reset_digest, @user.reload.reset_digest
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    assert_not flash.empty?
+    assert_redirected_to root_url
+    # パスワード再設定フォームのテスト
+    user = assigns(:user)
+    # メールアドレスが無効な場合
+    get edit_password_reset_path(user.reset_token, email: "")
+    assert_redirected_to root_url
+    # 無効なユーザー
+    user.toggle!(:activated)
+    get edit_password_reset_path(user.reset_token, email: user.email)
+    assert_redirected_to root_url
+    user.toggle!(:activated)
+    # メールアドレスが有効、トークンが無効
+    get edit_password_reset_path('wrong token', email: user.email)
+    assert_redirected_to root_url
+    # メールアドレスもトークンも有効
+    get edit_password_reset_path(user.reset_token, email: user.email)
+    assert_template 'password_resets/edit'
+    assert_select "input[name=email][type=hidden][value=?]", user.email
+    # 無効なパスワードとパスワード確認
+    patch password_reset_path(user.reset_token), params: {email: user.email,
+          user: {password: "foobar", password_confirmation: "foobaz"}}
+    assert_select '.alert'
+    # パスワードが空
+    patch password_reset_path(user.reset_token), params: {email: user.email, 
+          user: {password: "", password_confirmation: ""}}
+    assert_select '.alert'
+    # 有効なパスワードとパスワード確認
+    patch password_reset_path(user.reset_token), params: {email: user.email, 
+          user: {password: "foobar", password_confirmation: "foobar"}}
+    assert is_logged_in?
+    assert_not flash.empty?
+    assert_redirected_to user
+    assert_nil user.reload.reset_digest
+  end
+
+  test "expired token" do
+    get new_password_reset_path
+    post password_resets_path, params: {password_reset: {email: @user.email}}
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 3.hours.ago)
+    patch password_reset_path(@user.reset_token), params: {email: @user.email,
+          user: {password: "foobar", password_confirmation: "foobar"}}
+    assert_response :redirect
+    follow_redirect!
+    assert_match /expired/i, response.body
+  end
+end
