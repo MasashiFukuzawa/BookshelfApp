26ca175557bec14866ebc150724f66079f014419
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-01-17 13:02:09 +0900

    Modify the migration files

--- db/migrate/20190116142243_change_datatype_relationships_of_follower_id.rb
+++ db/migrate/20190116142243_change_datatype_relationships_of_follower_id.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeRelationshipsOfFollowerId < ActiveRecord::Migration[5.2]
   def change
-    change_column :relationships, :follower_id, :integer
+    change_column :relationships, :follower_id, :'integer USING CAST(follower_id AS integer)'
   end
 end
--- db/migrate/20190116142252_change_datatype_relationships_of_followed_id.rb
+++ db/migrate/20190116142252_change_datatype_relationships_of_followed_id.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeRelationshipsOfFollowedId < ActiveRecord::Migration[5.2]
   def change
-    change_column :relationships, :followed_id, :integer
+    change_column :relationships, :followed_id, :'integer USING CAST(followed_id AS integer)'
   end
 end
