0b7c1f1b3f53cad944c0ab8b9f83d349b283c481
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-01-20 10:37:47 +0900

    Tweak application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
 
-  def full_title(page_title = yield(:title))
-    base_title = "Bookshelf App"
+  def full_title(page_title = "", base_title = "Bookshelf App")
     return base_title if page_title.empty?
     "#{page_title} | #{base_title}"
   end
